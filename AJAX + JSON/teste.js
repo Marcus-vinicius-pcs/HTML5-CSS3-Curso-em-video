aa582e852304cca4bd4bfa3b4e246e97dd57dbf3
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-09-30 17:50:33 -0300

    teste

---
+++ AJAX + JSON/teste.js
@@ -0,0 +1,42 @@
+/*
+-->Selecionar a header e a section do documento.
+-->Armazenar a url de request 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json'
+--> Criar um novo request
+--> Abrir essa request
+--> colocar o tipo de texto dessa request 
+--> Enviar o request
+*/
+
+
+
+/*
+--> Criar uma função que vai funcionar quando carregar a página 
+--> Armazenar o texto do request em uma constante
+--> Criar uma função populateHeader que recebe como parâmetro a constante 
+--> Criar uma função showHeroes que recebe a constante como parâmetro
+*/
+
+/*
+na função populateHeader que recebe um obj copmo parametro, eu vou criar um h1 no documento e armazená-lo em uma variiuável 
+--> colocar nesse h1 o squadname do objeto
+--> e fazer com que o h1 fique dentro do header
+
+Criar um parágrafo que irá armazenar a frase 'Hometown: ' + obj['homeTown'] + ' // Formed: ' + obj['formed']
+Colocar esse parágrafo dentro da header
+
+*/
+
+/*
+showHeroes(obj)
+armazenar os membros desse objeto em uma constante
+
+Criar um laço que percorrerá toda essa constante e que para cada membro irá criar um artigo, um h2, 3 parágrafos e uma lista não ordenada
+
+Cada h2 irá mostrar o poder de cada objeto da constante
+O primeiro parágrafo irá mostrar a idendtidade secreta, o segundo a idade e o terceiro superpoderes
+
+Criar uma constante que armazena´ra os poderes de cada herói 
+Criar um laço quye irá percorrer esss poderes e preencher cada list item com esses poderes (criuar uma constante para os li) e deopois colocá-los dentro da lista 
+
+Depois fazer com que tudo isso fique dentro do artigo e, por fim, que o artigo fique dentro da constante 
+*/
