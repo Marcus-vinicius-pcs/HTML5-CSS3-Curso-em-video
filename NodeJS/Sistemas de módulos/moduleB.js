910f63ffae157783302ffded35255721be39e0e3
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-09-30 19:39:41 -0300

    sistemas de modulos em node

---
+++ NodeJS/Sistemas de módulos/moduleB.js
@@ -0,0 +1,4 @@
+//criando e exportando um modulo dinamicamente
+this.ola = 'Fala pessoal!'
+exports.bemVindo = 'Sejam bem vindos ao node'
+module.exports.ateLogo = 'até o próximo exemplo'
---
+++ NodeJS/Sistemas de módulos/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloAS')
+const moduloB = require('./moduleB')
+//importando um modulo
+
+console.log(moduloB.ola)
+console.log(moduloB.bemVindo)
+console.log(moduloB.ateLogo)
+console.log(moduloA.bomDia)
+console.log(moduloA.boaNoite())
+console.log(moduloA)
+console.log(moduloB)
